1c090d39dec33085f7932be6c1d74e51e736244e
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-05-21 23:18:58 -0600

    apply firebase and point to app in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rightatseven/firebase_options.dart';
+import 'package:rightatseven/src/app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World'))),
-    );
-  }
+  // Initialize the app with ProviderScope for Riverpod state management
+  runApp(const ProviderScope(child: App()));
 }
